6e83cbf6225855fd2b51007c55060e6c030ee308
Skaazi <lawman_mystic_0j@icloud.com>
2022-08-06 23:40:00 -0600

    Get rocket booster and use autoattacks

--- src/batfellow.ts
+++ src/batfellow.ts
@@ -1,6 +1,5 @@
 import { itemAmount, myHp, runChoice, takeStorage, use, visitUrl } from "kolmafia";
-import { $item, $location, $skill, get, Macro } from "libram";
-import { adventure } from "./combat";
+import { $item, $skill, get, Macro } from "libram";
 import { withContext } from "./lib";
 
 const choiceAdventures = [
@@ -20,23 +19,28 @@ export function batfellow() {
 function runBatfellow() {
   const comic = $item`Batfellow comic`;
   if (itemAmount(comic) < 1 && !takeStorage(1, comic)) throw `Missing batfellow comic?`;
+  new Macro()
+    .skill($skill`Bat-Kick`)
+    .repeat()
+    .setAutoAttack();
   use(1, comic); // Batfellow Begins
   visitUrl("place.php?whichplace=batman_cave&action=batman_cave_rnd");
   /*
     1: Bat-Suit upgrades
     3: Extra-Swishy Cloak (Always get the jump)
     11: Back to Main R&D Menu
+    2: Bat-Sedan Upgrades (Reduce travel time by 5 minutes)
+    1: Rocket Booster
+    11: Back to Main R&D Menu
     3: Bat-Cavern upgrades
     4: Surveillance Network (Fights cost 1 minute less)
     11: Back to Main R&D Menu
     6: Stop Researching and Developing
   */
-  for (const c of [1, 3, 11, 3, 4, 11, 6]) runChoice(c);
+  for (const c of [1, 3, 11, 2, 1, 11, 3, 4, 11, 6]) runChoice(c);
   visitUrl("place.php?whichplace=batman_cave&action=batman_cave_car");
   runChoice(5); // Center Park
-  while (myHp() > 0 && get("batmanTimeLeft") >= 4) {
-    adventure($location`Center Park After Dark`, new Macro().skill($skill`Bat-Kick`).repeat());
-  }
+  while (myHp() > 0 && get("batmanTimeLeft") >= 4) visitUrl("adventure.php?snarfblat=461");
   visitUrl("place.php?whichplace=batman_park&action=batman_park_car");
   runChoice(9); // EJECT
 }
